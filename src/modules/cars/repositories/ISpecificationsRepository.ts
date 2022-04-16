108857e89e65e6aa32803e77b8c2fae671ae1565
Camilla Correia <camillajesuscorreia@hotmail.com>
2022-04-16 11:25:25 -0300

    refactor: category and specification dto

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -1,9 +1,5 @@
+import { ICreateSpecificationDTO } from '../dtos/ICreateSpecificationDTO';
 import { Specification } from '../entities/Specification';
-
-interface ICreateSpecificationDTO {
-  name: string;
-  description: string;
-}
 
 interface ISpecificationsRepository {
   create({
@@ -14,4 +10,4 @@ interface ISpecificationsRepository {
   findByName(name: string): Promise<Specification>;
 }
 
-export { ISpecificationsRepository, ICreateSpecificationDTO };
+export { ISpecificationsRepository };
