341fc22ad7e605c525dff208afaedd5121834503
Camilla Correia <camillajesuscorreia@hotmail.com>
2022-05-17 16:26:05 -0300

    feat: create car

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,55 @@
+import { Category } from '@modules/cars/infra/typeorm/entities/Category';
+import {
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  JoinColumn,
+  Entity,
+  ManyToOne,
+} from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('cars')
+class Car {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  daily_rate: number;
+
+  @Column()
+  available: true;
+
+  @Column()
+  license_plate: string;
+
+  @Column()
+  fine_amount: number;
+
+  @Column()
+  brand: string;
+
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
+  category_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
+
+export { Car };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,9 @@
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 import { container } from 'tsyringe';
@@ -23,3 +25,6 @@ container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
 );
+
+// ICarsRepository
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
